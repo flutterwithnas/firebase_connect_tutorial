8e40f2d2b30a6ef9b1459d8fc78a95ab0d1eab4b
Nas <nas.shihab92@gmail.com>
2023-07-13 20:46:41 +0600

    connect ok Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -11,7 +15,30 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Placeholder(),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              FirebaseAuth.instance.createUserWithEmailAndPassword(
+                email: 'flutter@mail.com',
+                password: '123456',
+              );
+            },
+            child: const Text('Test Firebase'),
+          ),
+        ),
+      ),
     );
   }
 }
